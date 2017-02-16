6eb7bcbfff26c7a768ae2edd1d6ffa6b96086aa7
agarzonp <agarzonp@gmail.com>
2017-02-16 17:35:47 +0100

    Basic Math header that includes a not full vec3 sctruct and a distance function added.

--- OptimizationTechniques/OptimizationTechniques/main.cpp
+++ OptimizationTechniques/OptimizationTechniques/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+#include "math/Math.h"
+
 int main()
 {
+	agarzon::Vec3 a;
+	agarzon::Vec3 b(10.0f, 0.0f, 0.0f);
+	agarzon::Vec3 c(2.0f, 2.0f, 2.0f);
+
+	std::cout << agarzon::Distance(a, b) << " " << agarzon::Distance(a, c) << " " << agarzon::Distance(b, c);
+
 	std::cout << "Press any key to continue";
 	std::getchar();
 	return 0;
---
+++ OptimizationTechniques/OptimizationTechniques/src/math/Math.h
@@ -0,0 +1,49 @@
+#ifndef AGARZON_MATH_H
+#define AGARZON_MATH_H
+
+#include <cmath>
+
+namespace agarzon
+{
+	struct Vec3
+	{
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+
+		Vec3() {}
+		Vec3(float x_, float y_, float z_) : x(x_), y(y_), z(z_) {}
+
+
+		Vec3& operator=(const Vec3& rhs)
+		{
+			x = rhs.x;
+			y = rhs.y;
+			z = rhs.z;
+		}
+
+		Vec3 operator-(const Vec3& rhs) const
+		{
+			return Vec3(x - rhs.x, y - rhs.y, z - rhs.z);
+		}
+
+		Vec3 operator+(const Vec3& rhs) const
+		{
+			return Vec3(x + rhs.x, y + rhs.y, z + rhs.z);
+		}
+
+		float Magnitude() const
+		{
+			return std::sqrt(x*x + y*y + z*z);
+		}
+
+		// Note: Add more vector functionality as needed
+	};
+
+	float Distance(const Vec3& a, const Vec3& b)
+	{
+		return (b - a).Magnitude();
+	}
+}
+
+#endif
